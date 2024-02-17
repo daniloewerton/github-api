5d9927c44365280664a90476aa94556cbfea29a2
Danilo Ewerton <danilo.ewe@gmail.com>
2024-02-17 11:14:19 -0300

    criação da página home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+function Home() {
+  return (
+    <div className="main-container">
+      <h1>Desafio Github API</h1>
+      <p>DevSuperior - Escola de programação</p>
+    </div>
+  );
+}
+
+export default Home;
