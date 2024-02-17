d99c803267884e0907a77cae48108e50cbd592c7
Danilo Ewerton <danilo.ewe@gmail.com>
2024-02-17 11:04:58 -0300

    removendo códigos desnecessários

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,7 +3,7 @@ import './styles.css';
 function Navbar() {
   return (
     <nav className="navbar bg-primary navbar-area">
-      <div className="container-fluid nav-logo">
+      <div className="container-fluid">
         <a href="/">
           <h4>Github API</h4>
         </a>
